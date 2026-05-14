dca9467f5d2ea76de7633c5cf878b323d092b27a
JESUS MARTIN GARCIA RAMIREZ <jmgramirez@minsait.com>
2026-05-13 18:34:30 -0600

    feat: agregar lógica para guardar registros de soporte y enlazar formularios

--- src/app/dashboard/registros/nuevo-registro-soporte/nuevo-registro-soporte.ts
+++ src/app/dashboard/registros/nuevo-registro-soporte/nuevo-registro-soporte.ts
@@ -1,10 +1,24 @@
 import { Component } from '@angular/core';
 import { Boton } from '../../../compartida/boton/boton';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-nuevo-registro-soporte',
-  imports: [Boton],
+  imports: [Boton, FormsModule],
   templateUrl: './nuevo-registro-soporte.html',
   styleUrl: './nuevo-registro-soporte.css',
 })
-export class NuevoRegistroSoporte {}
+export class NuevoRegistroSoporte {
+
+  titulo: string = '';
+  pedido: string = '';
+
+  guardarRegistro() {
+    // Aquí iría la lógica para guardar el nuevo registro de soporte
+    console.log('Registro de soporte guardado');
+    console.log('Título:', this.titulo);
+    console.log('Pedido:', this.pedido);
+    this.titulo = '';
+    this.pedido = '';
+  }
+} 
