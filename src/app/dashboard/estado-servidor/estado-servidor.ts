3d6cdd7478a2107422e6e306803f793615a6a0fd
JESUS MARTIN GARCIA RAMIREZ <jmgramirez@minsait.com>
2026-05-13 18:16:11 -0600

    feat: mejorar la lógica de estado del servidor y actualizar el template

--- src/app/dashboard/estado-servidor/estado-servidor.ts
+++ src/app/dashboard/estado-servidor/estado-servidor.ts
@@ -1,11 +1,19 @@
-import { Component, Input } from '@angular/core';
+import { Component, OnInit, signal } from '@angular/core';
 
 @Component({
   selector: 'app-estado-servidor',
   imports: [],
   templateUrl: './estado-servidor.html',
-  styleUrl: './estado-servidor.css',
+  styleUrls: ['./estado-servidor.css'],
 })
-export class EstadoServidor {
-  estadoActual = 'online';
+export class EstadoServidor implements OnInit {
+  estadoActual = signal<'online' | 'offline' | 'unknown'>('unknown');
+  private readonly estados = ['online', 'offline', 'unknown'] as const;
+
+  ngOnInit() {
+    setInterval(() => {
+      const opciones = this.estados.filter((estado) => estado !== this.estadoActual());
+      this.estadoActual.set(opciones[Math.floor(Math.random() * opciones.length)]);
+    }, 3000);
+  }
 }
